0c0e847a34739e22b8354b2f2008a2a1408e8211
condef5 <frank@able.co>
2020-06-29 21:48:55 -0500

    Init project :tada:

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a Anime Adventure</h1>
+      <p>Directorio de los mejores animes jamás nonbrados.</p>
+    </div>
+  );
+}
+
+export default Home;
